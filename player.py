6dc23b94a8a2e78fe3f87476d193c515e3a23002
humanbeing-dev <maciej.sitowski@gmail.com>
2020-11-09 08:44:43 +0100

    Refactor application
    Add new class VolumeFrame that handles volume functionality
    Add new ImageConverter class that resizes and rotates images

--- player.py
+++ player.py
@@ -10,7 +10,7 @@ import pickle
 
 
 class MusicPlayer(Frame):
-    """Application that loads and play music"""
+    """Application that loads and plays music"""
     def __init__(self, master):
         pygame.mixer.init()
         super().__init__(master)
@@ -52,24 +52,12 @@ class MusicPlayer(Frame):
         """Create all frames and images"""
         self.list_frame = Frame(self.master)
         self.list_frame.grid(row=0, column=0)
-        self.volume_frame = LabelFrame(self.master, text="Volume")
-        self.volume_frame.grid(row=0, column=1, rowspan=2)
+
+        self.volume_frame = VolumeFrame(master=self.master, text="Volume")
+        self.volume_frame.grid(row=0, column=2, rowspan=2)
+
         self.btn_frame = Frame(self.master)
         self.btn_frame.grid(row=1, column=0, padx=20)
-
-        global vol_img_1, vol_img_2, vol_img_3, vol_img_4, vol_img_5
-        vol_img_1 = self.create_image(f'img/volume1.png', 0)
-        vol_img_2 = self.create_image(f'img/volume2.png', 0)
-        vol_img_3 = self.create_image(f'img/volume3.png', 0)
-        vol_img_4 = self.create_image(f'img/volume4.png', 0)
-        vol_img_5 = self.create_image(f'img/volume5.png', 0)
-
-        self.volume_slider = ttk.Scale(self.volume_frame, value=.25, from_=1, to=0, orient=VERTICAL, command=self.volume, length=200)
-        self.volume_slider.grid(row=1, column=0)
-        self.volume_label = Label(self.volume_frame, text=f"{(self.volume_slider.get()):.02f}", anchor='center')
-        self.volume_label.grid(row=0, column=0)
-        self.volume_image = Label(self.volume_frame, image=vol_img_5, anchor=S)
-        self.volume_image.grid(row=2, column=0, sticky=S)
 
         self.listbox = Listbox(self.list_frame, bg='black', fg='green', selectbackground='gray', selectforeground='black', selectmode=EXTENDED, width=48)
         self.listbox.pack(fill=X, padx=20, pady=(20, 0))
@@ -78,11 +66,11 @@ class MusicPlayer(Frame):
 
         # setting images
         global img_1, img_2, img_3, img_4, img_5
-        img_1 = self.create_image(f'img/next.png', 180)
-        img_2 = self.create_image(f'img/play.png', 0)
-        img_3 = self.create_image(f'img/pause.png', 0)
-        img_4 = self.create_image(f'img/stop.png', 0)
-        img_5 = self.create_image(f'img/next.png', 0)
+        img_1 = ImageConverter.create_image(f'img/next.png', 40, 180)
+        img_2 = ImageConverter.create_image(f'img/play.png', 40, 0)
+        img_3 = ImageConverter.create_image(f'img/pause.png', 40, 0)
+        img_4 = ImageConverter.create_image(f'img/stop.png', 40, 0)
+        img_5 = ImageConverter.create_image(f'img/next.png', 40, 0)
 
         images = [{"file": img_1, "command": self.previous},
                   {"file": img_2, "command": self.play},
@@ -114,26 +102,24 @@ class MusicPlayer(Frame):
 
     def add_song(self):
         """Open dialogbox, chose one file and load it"""
-        start_path = pathlib.Path().absolute()
-        song = filedialog.askopenfilename(
-            initialdir=start_path,
-            title="Choose a song",
-            filetypes=(("flac files", "*.flac"),
-                       ("mp3 files", "*.mp3"),
-                       ("all files", "*")))
+        settings = dict(initialdir=pathlib.Path().absolute(), title="Choose songs", filetypes=(
+            ("flac files", "*.flac"),
+            ("mp3 files", "*.mp3"),
+            ("all files", "*")))
+
+        song = filedialog.askopenfilename(**settings)
 
         self.update_playlist(song)
         self.listbox.insert("end", self.song_list[-1]['name'])
 
     def add_songs(self):
         """Open dialogbox, chose multiple files and load them"""
-        start_path = pathlib.Path().absolute()
-        songs = filedialog.askopenfilenames(
-            initialdir=start_path,
-            title="Choose songs",
-            filetypes=(("flac files", "*.flac"),
-                       ("mp3 files", "*.mp3"),
-                       ("all files", "*")))
+        settings = dict(initialdir=pathlib.Path().absolute(), title="Choose songs", filetypes=(
+            ("flac files", "*.flac"),
+            ("mp3 files", "*.mp3"),
+            ("all files", "*")))
+
+        songs = filedialog.askopenfilenames(**settings)
 
         for song in songs:
             self.update_playlist(song)
@@ -221,24 +207,9 @@ class MusicPlayer(Frame):
         self.stopped = True
 
     def slide(self, event):
-        # position = int(self.slider.get())
-        # fposition = time.strftime('%M:%S', time.gmtime(position))
-        # self.info_label.config(text=f"{fposition} of {song_to_play['flength']}")
-
         song_to_play = self.get_song()
         pygame.mixer.music.load(song_to_play['path'])
         pygame.mixer.music.play(loops=0, start=int(self.slider.get()))
-
-    def volume(self, event):
-        volume_level = self.volume_slider.get()
-        pygame.mixer.music.set_volume(volume_level)
-        self.volume_label.config(text=f"{volume_level:.02f}")
-
-        if volume_level == 0: self.volume_image.config(image=vol_img_1)
-        elif volume_level <= .25: self.volume_image.config(image=vol_img_2)
-        elif volume_level <= .50: self.volume_image.config(image=vol_img_3)
-        elif volume_level <= .75: self.volume_image.config(image=vol_img_4)
-        elif volume_level <= 1: self.volume_image.config(image=vol_img_5)
 
     def song_time(self, song):
         if self.stopped:
@@ -268,6 +239,50 @@ class MusicPlayer(Frame):
             self.slider.config(to=length, value=next_time)
 
         self.info_label.after(1000, lambda: self.song_time(song))
+
+
+class VolumeFrame(LabelFrame):
+    def __init__(self, **kw):
+        super().__init__(**kw)
+        self.create_elements()
+        self.create_images()
+
+    def create_elements(self):
+        self.slider = ttk.Scale(self, value=.25, from_=1, to=0, orient=VERTICAL, command=self.volume,length=200)
+        self.slider.grid(row=1, column=0)
+        self.label = Label(self, text=f"{(self.slider.get()):.02f}", anchor='center')
+        self.label.grid(row=0, column=0)
+        self.image = Label(self, image=vol_img_5, anchor=S)
+        self.image.grid(row=2, column=0, sticky=S)
+
+    @staticmethod
+    def create_images(self):
+        global vol_img_1, vol_img_2, vol_img_3, vol_img_4, vol_img_5
+        vol_img_1 = ImageConverter.create_image(f'img/volume1.png', 50)
+        vol_img_2 = ImageConverter.create_image(f'img/volume2.png', 50)
+        vol_img_3 = ImageConverter.create_image(f'img/volume3.png', 50)
+        vol_img_4 = ImageConverter.create_image(f'img/volume4.png', 50)
+        vol_img_5 = ImageConverter.create_image(f'img/volume5.png', 50)
+
+    def volume(self, volume_level):
+        volume_level = float(volume_level)
+        pygame.mixer.music.set_volume(volume_level)
+        self.label.config(text=f"{volume_level:.02f}")
+
+        if volume_level == 0: self.image.config(image=vol_img_1)
+        elif volume_level <= .25: self.image.config(image=vol_img_2)
+        elif volume_level <= .50: self.image.config(image=vol_img_3)
+        elif volume_level <= .75: self.image.config(image=vol_img_4)
+        elif volume_level <= 1: self.image.config(image=vol_img_5)
+
+
+class ImageConverter:
+    @staticmethod
+    def create_image(path, size, rotation=0):
+        img = Image.open(path)
+        img = img.rotate(rotation)
+        img = img.resize((size, size))
+        return ImageTk.PhotoImage(img)
 
 
 if __name__ == '__main__':
